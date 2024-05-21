3f87a8fd0004095903a5c5de002c6b4942527830
Marshal <mpetry2@hotmail.com>
2024-05-21 20:26:32 -0300

    initial commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: "1988",
+    description: "С днем рождения, Комсомол! (Happy Birthday, Komsomol!)",
+    src: "01.jpg",
+    link: "https://repository.duke.edu/dc/russianposters/rpcps06016",
+    color: "#8c8670"
+  },
+  {
+    title: "1988",
+    description: "Правильно понять прошлое -- значит облегчить поиски верного пути в будущее (Correctly understanding the past eases the search for a true path to the future)",
+    src: "02.jpg",
+    link: "https://repository.duke.edu/dc/russianposters/rpcps06013",
+    color: "#b7cfae"
+  },
+  {
+    title: "unknown",
+    description: "Приезд В.И. Ленина в Петроград 1917 г. (The Coming of Lenin to Petrograd in 1917)",
+    src: "03.jpg",
+    link: "https://repository.duke.edu/dc/russianposters/rpcps03028",
+    color: "#db5b00"
+  },
+  {
+    title: "unknown",
+    description: "На борьбу с самогоном! (Fighting Against Illegal Alcohol)",
+    src: "04.jpg",
+    link: "https://repository.duke.edu/dc/russianposters/rpcps03025",
+    color: "#8f0000"
+  },
+  {
+    title: "unknown",
+    description: "то дала крестьянству диктатура пролетариата (what the dictatorship of the proletariat gave to the proletariat.)",
+    src: "05.jpg",
+    link: "https://repository.duke.edu/dc/russianposters/rpcps02011",
+    // color: "#88A28D"
+    color: "#679267"
+  }
+]
